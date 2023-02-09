cebe3c152005b9c3339101c7b5281a710ffcf6c2
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-02-09 16:18:48 +0100

    show menu and modal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "./Sidebar";
 import Modal from "./Modal";
 import { FaBars } from "react-icons/fa";
 
 export default function App() {
+  const [showSideBar, setShowSideBar] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className="main-con">
-      <div className="overlay"></div>
-      <FaBars className="menu-icon" />
-      <Sidebar />
-      <button className="modal-btn">Show Modal</button>
-      <Modal />
+      {showModal && <div className="overlay" onClick={()=>setShowModal(prev => !prev)}></div>}
+      <FaBars
+        className="menu-icon"
+        onClick={() => setShowSideBar(!showSideBar)}
+      />
+      <Sidebar showSideBar={showSideBar} setShowSideBar={setShowSideBar} />
+      <button className="modal-btn" onClick={() => setShowModal(!showModal)}>
+        Show Modal
+      </button>
+      <Modal showModal={showModal} setShowModal={setShowModal} />
     </div>
   );
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,49 +1,37 @@
 import React from "react";
 import logo from "./logo.svg";
 import { FaTimes, FaHome, FaBehance } from "react-icons/fa";
+import { links, social } from "./Data";
 
-export default function Sidebar() {
-  return (
-    <nav className="sidebar-con">
-      <div className='sidebar-head'>
+export default function Sidebar({showSideBar, setShowSideBar}) {
+  return ( 
+    <nav className={`sidebar-con ${showSideBar && 'show'}`}>
+      <div className="sidebar-head">
         <img src={logo} alt="logo" className="logo" />
-        <FaTimes className="cancel"/>
+        <FaTimes className="cancel" onClick={()=>setShowSideBar(prev => !prev)}/>
       </div>
       <div className="menu-con">
         <ul>
-          <li>
-            <a href="#">
-              <FaHome /> Home
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <FaHome /> Home
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <FaHome /> Home
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <FaHome /> Home
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <FaHome /> Home
-            </a>
-          </li>
+          {links.map((link) => {
+            const {id, url, text, icon} = link
+            return (
+              <li key={id}>
+                <a href="#">
+                  <span className="item-icon">{icon}</span> {text[0].toUpperCase() + text.slice(1)}
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className="icon-con">
-            <FaBehance/>
-            <FaBehance/>
-            <FaBehance/>
-            <FaBehance/>
-            <FaBehance/>
+        {social.map((socialIcon, index) => {
+          const {id, url, icon} = socialIcon
+          return <a href="#" key={index}>
+            <span className='social-icon'>{icon}</span>
+        </a>
+        })}
+        
       </div>
     </nav>
   );
